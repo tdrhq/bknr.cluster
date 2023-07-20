968fbd12993a92243cf6dbebcac0f09ad104d5e4
Arnold Noronha <arnold@tdrhq.com>
2023-07-20 13:18:54 -0400

    First compilation infrastructure for braft based cluster
    Summary:
    ...
    
    ...
    
    Differential Revision: https://phabricator.tdrhq.com/D7538
    
    GitOrigin-RevId: 3631f9ecc105d9ebf8753fcb21f9f43afa65c01c

---
+++ braft_compat.cpp
@@ -0,0 +1,27 @@
+/* -*- c-basic-offset: 2; -*- */
+
+#include <brpc/controller.h>       // brpc::Controller
+#include <brpc/server.h>           // brpc::Server
+#include <braft/raft.h>                  // braft::Node braft::StateMachine
+#include <braft/storage.h>               // braft::SnapshotWriter
+#include <braft/util.h>                  // braft::AsyncClosureGuard
+#include <braft/protobuf_file.h>         // braft::ProtoBufFile
+
+namespace bknr {
+  class BknrStateMachine : public braft::StateMachine {
+    int lispHandle;
+  public:
+    BknrStateMachine (int _lispHandle) : lispHandle(_lispHandle) {
+    }
+
+    void on_apply(::braft::Iterator& iter) {
+    }
+  };
+
+
+  extern "C" {
+    BknrStateMachine* make_bknr_state_machine(int lispHandle) {
+      return new BknrStateMachine(lispHandle);
+    }
+  }
+}
