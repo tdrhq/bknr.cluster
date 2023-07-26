f87fc9141c602ea48e97034b265c15824063cf9c
Arnold Noronha <arnold@tdrhq.com>
2023-07-26 11:15:24 -0400

    First save and restore test
    Summary:
    It's not perfect.. I think I need callbacks from
    leader_start, and leader_stop.
    
    Differential Revision: https://phabricator.tdrhq.com/D7575
    
    GitOrigin-RevId: 69a079ef4c21be13378ee1dd45a313c1aac8393a

--- test-store.lisp
+++ test-store.lisp
@@ -2,6 +2,8 @@
   (:use #:cl
         #:fiveam)
   (:import-from #:bknr.datastore
+                #:close-store-object
+                #:*store*
                 #:execute-transaction
                 #:class-instances
                 #:close-store
@@ -15,7 +17,11 @@
   (:import-from #:bknr.cluster/store
                 #:cluster-store)
   (:import-from #:bknr.cluster/server
-                #:with-logs-hidden))
+                #:with-logs-hidden)
+  (:import-from #:util/store/store
+                #:clear-indices-for-tests)
+  (:import-from #:fiveam-matchers/has-length
+                #:has-length))
 (in-package :bknr.cluster/test-store)
 
 (util/fiveam:def-suite)
@@ -28,19 +34,32 @@
 (defclass my-test-store (cluster-store)
   ())
 
+(defun safe-close-store ()
+  (when (boundp '*store*)
+   (let ((store *store*))
+     (close-store)
+     (close-store-object store))))
+
+
 (def-fixture state ()
   (with-logs-hidden ()
    (tmpdir:with-tmpdir (dir)
      (let* ((port (util/random-port:random-port)))
        (unwind-protect
-            (let ((store (make-instance 'my-test-store
+            (let (store)
+              (labels ((restore ()
+                         (safe-close-store)
+                         (open-store))
+                       (open-store ()
+                         (setf store (make-instance 'my-test-store
                                         :election-timeout-ms 100
                                         :directory dir
                                         :group "dummy"
                                         :config (format nil "127.0.0.1:~a:0" port)
-                                        :port port)))
-              (&body))
-         (close-store))))))
+                                        :port port))))
+                (open-store)
+                (&body)))
+         (safe-close-store))))))
 
 (test simple-creation
   (with-fixture state ()
@@ -50,3 +69,16 @@
       (setf (arg obj) 2)
       (assert-that (arg obj)
                    (is-equal-to 2)))))
+
+(test simple-restore
+  (with-fixture state ()
+    (let ((obj (make-instance 'foo)))
+      (assert-that (class-instances 'foo)
+                   (contains obj))
+      (safe-close-store)
+      (assert-that (class-instances 'foo)
+                   (contains))
+      (open-store)
+      (sleep 1) ;; TODO: remove
+      (assert-that (class-instances 'foo)
+                   (has-length 1)))))
