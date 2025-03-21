1e3019f5db69913c6d47f45d308724ad98c827a3
Arnold Noronha <arnold@screenshotbot.io>
2025-03-20 20:39:01 -0400

    Revert "Don't use ::braft::cli for leadership transfer"
    This reverts commit e95370911d6911058f4f5c7cb391862675198017.
    
    GitOrigin-RevId: 90df7939bc37d76066b222f7b4f65ae2ef484a45

--- braft_compat.cpp
+++ braft_compat.cpp
@@ -370,10 +370,8 @@ public:
       ::braft::Configuration c(peers);
       braft::PeerId p = peers[rand() % peers.size()];
 
-      int res = fsm->_node->transfer_leadership_to(p);
-      if (res == -1) {
-          LOG(ERROR) << "Leadership transfer failed";
-      }
+      ::braft::cli::CliOptions cliOptions;
+      ::braft::cli::transfer_leader(nodeId.group_id, c, p, cliOptions);
     }
 
     // if peerStr is "0.0.0.0:0:0", then it will randomly transfered to the
